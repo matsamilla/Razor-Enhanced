ef4c21b97e03b9b84b1c51c279dc053f9b8afed6
matsamilla <45471473+matsamilla@users.noreply.github.com>
2018-12-07 08:36:10 -0700

    Update BigHeal.py

--- BigHeal.py
+++ BigHeal.py
@@ -1,6 +1,11 @@
-if Player.Poisoned:
-    Spells.CastMagery('Cure')
-    Target.SelfQueued()
-else:
-    Spells.CastMagery('Greater Heal')
-    Target.SelfQueued()
+Target.ClearLastandQueue()
+Target.Cancel()
+Spells.Interrupt()
+Misc.Pause(600)
+if not Target.HasTarget():
+    if Player.Poisoned:
+        Spells.CastMagery('Cure')
+        Target.SelfQueued()
+    else:
+        Spells.CastMagery('Greater Heal')
+        Target.SelfQueued()

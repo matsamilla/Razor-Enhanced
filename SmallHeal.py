92f6ddcb3a5f4fd710f6b766c1575cdeb8db3ce8
matsamilla <45471473+matsamilla@users.noreply.github.com>
2019-02-01 09:32:17 -0700

    Update SmallHeal.py

--- SmallHeal.py
+++ SmallHeal.py
@@ -1,11 +1,10 @@
-Target.ClearLastandQueue()
-Target.Cancel()
-Spells.Interrupt()
-Misc.Pause(600)
 if not Target.HasTarget():
     if Player.Poisoned:
-        Spells.CastMagery('Cure')
-        Target.SelfQueued()
+        Spells.CastMagery('Cure', True)
+        Target.WaitForTarget(1500)
+        Target.Self()
     else:
-        Spells.CastMagery('Greater Heal')
-        Target.SelfQueued()
+        Spells.CastMagery('Heal', True)
+        Target.WaitForTarget(1500)
+        Target.Self()
+     

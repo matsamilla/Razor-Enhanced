d896313e89155e17bc194444dc21e316be4aff48
matsamilla <45471473+matsamilla@users.noreply.github.com>
2019-08-22 10:48:58 -0600

    Update BigHeal.py

--- BigHeal.py
+++ BigHeal.py
@@ -1,9 +1,9 @@
 if not Target.HasTarget():
     if Player.Poisoned:
-        Spells.CastMagery('Cure', True)
+        Spells.CastMagery('Cure')
         Target.WaitForTarget(1500)
         Target.Self()
     else:
-        Spells.CastMagery('Greater Heal', True)
+        Spells.CastMagery('Greater Heal')
         Target.WaitForTarget(1500)
         Target.Self()

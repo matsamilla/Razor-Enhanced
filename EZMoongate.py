ab1008ef66dbc5ef70360ff066bf8400cc128af5
matsamilla <45471473+matsamilla@users.noreply.github.com>
2020-04-25 16:40:34 -0600

    Update EZMoongate.py

--- EZMoongate.py
+++ EZMoongate.py
@@ -1,4 +1,5 @@
-#moongate clicker
+# This will use a moongate within 2 tiles, and accept the gump if there is one.
+# By MatsaMilla, updated 4/25/20
 
 from System.Collections.Generic import List 
 
@@ -8,10 +9,13 @@ gate.Enabled = True
 gate.OnGround = True
 gate.Movable = False
 gate.RangeMax = 2
+gate.Graphics = List[int]( [ 0x0F6C ] )
 moongate = Items.ApplyFilter(gate)
 
-for m in moongate: # Look for items in filter
-    if m.ItemID == 0x0F6C: # Return true if found a moongate = 0x0F6C
-        Items.UseItem(m) # Double click on the moongate found 
-        Gumps.WaitForGump(3716879466, 10000)
-        Gumps.SendAction(3716879466, 1)
+if moongate:
+    m = Items.Select( moongate , 'Nearest' )
+    Items.UseItem(m) # Double click on the moongate found 
+    Gumps.WaitForGump(3716879466, 1500)
+    Gumps.SendAction(3716879466, 1)
+else:
+    Misc.SendMessage('No Gate In Range',33)

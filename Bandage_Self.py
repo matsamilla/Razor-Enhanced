4c53fe8a5b7735097da65f0296e0126b4c3718a7
matsamilla <45471473+matsamilla@users.noreply.github.com>
2020-05-29 11:22:39 -0600

    Update Bandage_Self.py

--- Bandage_Self.py
+++ Bandage_Self.py
@@ -1,48 +1,87 @@
-# Bandage script by Matsamilla
-# Rely's on Bandage_Timer.py, without it it will not stop applying bandages.
+# Bandage Timer by Wardoc
+# Contributions by Matsamilla
+# last updated 5/29/20 - added overhead message option
+# I have this always running, start at login.
 
-# Pause at start for skill check,
-# Script stops if no healing on toon.
-Misc.Pause( 5000 )
-if Player.GetRealSkillValue( 'Healing' ) < 50:
-    Misc.SendMessage( 'No Healing, stopping script' )
-    Misc.Pause( 200 )
-    Stop
+# True for overhead Bandage messages, false for in system messages
+overheadMessage = False
 
-import sys
 
-def BandageSelf():
-    if Misc.ReadSharedValue('bandageDone') == True:
-        lastTarget = Target.GetLast()
-        while Target.HasTarget():
-            Misc.Pause(10)
-        Items.UseItemByID(0x0E21, 0)
-        Target.WaitForTarget(1500, False)
-        Target.Self()
-        Misc.Pause (500)
+
+msgcolor = 88
+Misc.SetSharedValue('bandageDone', True)
+def BandagesApplying():
+    # Fetch the Journal entries (oldest to newest)
+    regularText = Journal.GetTextByType( 'Regular' )
+
+    # Reverse the Journal entries so that we read from newest to oldest
+    regularText.Reverse()
+
+    # Read back until the bandages were started to see if they have finished applying
+    for line in regularText[ 0 : len( regularText ) ]:
+        if (line == 'You begin applying the bandages.' or
+                line == 'Your hands are still busy applying other bandages.'):
+            break
+        if ( line == 'You finish applying the bandages.' or
+                line == 'You heal what little damage your patient had.' or
+                line == 'You apply the bandages, but they barely help.' or
+                line == 'That being is not damaged!' or
+                line == 'You have cured the target of all poisons!' or
+                line == 'You are unable to resurrect your patient.' or 
+                line == 'You are able to resurrect your patient' or
+                line == 'You are able to resurrect the creature.' or
+                line == 'You fail to resurrect the creature.' or
+                line == 'You did not stay close enough to heal your patient!'):
+            return False
+    return True
+
+
+def WaitForBandagesToApply():
+    Misc.SetSharedValue('bandageDone', False)
+    #bandageDone = False
+    secondsCounter = 0
+    while BandagesApplying():
+        worldSave()
+        Misc.Pause( 1000 )
         
-        Target.SetLast(lastTarget)
+        if overheadMessage:
+            if secondsCounter == 0:
+                Player.HeadMessage( msgcolor, 'Bandage Started' )
+            else:
+                Player.HeadMessage (msgcolor, 'Bandage: %is' % ( secondsCounter ))
+        else:
+            if secondsCounter == 0:
+                Misc.SendMessage( 'Bandage Started' , msgcolor )
+            else:
+                Misc.SendMessage ( 'Bandage: %is' % ( secondsCounter ) , msgcolor)
+        
+        secondsCounter += 1
+        if secondsCounter > 18:
+            break
+            
+    if overheadMessage:
+        Player.HeadMessage(msgcolor, 'Bandage Available')
+    else:
+        Misc.SendMessage( 'Bandage Available', msgcolor )
+       
+    Misc.SetSharedValue('bandageDone', True)
+    return
 
-Timer.Create( 'outOfBandageWarningIncrement', 1 )
-while True:
-    # make sure Bandage_Timer.py is running, script won't run
-    # correctly if timer is not running.
-    if not Misc.ScriptStatus('Bandage_Timer.py'):
-        Misc.ScriptRun('Bandage_Timer.py')
+def worldSave():
+    if Journal.SearchByType('The world is saving, please wait.', 'Regular' ):
+        Misc.SendMessage('Pausing for world save', 33)
+        while not Journal.SearchByType('World save complete.', 'Regular'):
+            Misc.Pause(1000)
+        Misc.SendMessage('Continuing', 33)
+        Journal.Clear()    
     
+Journal.Clear()
+while True:
     if Player.IsGhost:
         # Player died, wait until Player is resurrected
         while Player.IsGhost:
             Misc.Pause( 100 )
-    
-    # Checks for bandages
-    if Items.BackpackCount( 0x0E21 ,-1 ):
-        # only apply bandage if player is not hidden, damaged or poisoned and not one already applying.
-        if Player.Visible == True and Player.Hits < Player.HitsMax or Player.Poisoned and Misc.CheckSharedValue('bandageDone') == True:
-            BandageSelf()
-    # if no bandages, warn player.
-    elif Timer.Check( 'outOfBandageWarningIncrement' ) == False:
-        Player.HeadMessage( 1100, 'Out of bandages!' )
-        Timer.Create( 'outOfBandageWarningIncrement', 5000 )
-
+    if Journal.Search('You begin applying the bandages.'):
+        WaitForBandagesToApply()
+        Journal.Clear()
     Misc.Pause( 50 )

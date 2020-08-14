d143aa0768cc894fad49067495752d401baee45b
matsamilla <45471473+matsamilla@users.noreply.github.com>
2020-08-14 08:20:26 -0600

    Update pvp_poisonWep.py

--- pvp_poisonWep.py
+++ pvp_poisonWep.py
@@ -1,8 +1,23 @@
-poisPot = Items.FindByID(0x0F0A,-1,Player.Backpack.Serial)
-if poisPot:
-    Player.UseSkill("Poisoning")
-    Target.WaitForTarget(10000, False)
-    Target.TargetExecute(poisPot.Serial)
-    righthand = Player.GetItemOnLayer('RightHand')
-    Target.WaitForTarget(10000, False)
-    Target.TargetExecute(righthand.Serial)
+if Player.GetItemOnLayer("RightHand"):
+    item = Player.GetItemOnLayer("RightHand")
+elif Player.GetItemOnLayer("LeftHand"):
+    item = Player.GetItemOnLayer("LeftHand")
+else:
+    Misc.SendMessage("No Weapon in Hand",48)
+
+Items.WaitForProps(item,2000)
+charges = Items.GetPropValue(item,"Charges")
+if charges > 0:
+    Player.HeadMessage(78,"{} poison charges".format(int(charges)))
+if charges < 1:
+    Player.HeadMessage(48,"Repoisoning, {} potions left".format(int(Items.BackpackCount(0x0F0A))))
+    pot = Items.FindByID(0x0F0A,-1,Player.Backpack.Serial)
+    if pot:
+        Player.UseSkill("Poisoning")
+        Target.WaitForTarget(2000)
+        Target.TargetExecute(pot)
+        Target.WaitForTarget(2000)
+        Target.TargetExecute(item)
+        Player.HeadMessage(48,"{} potions left".format(int(Items.BackpackCount(0x0F0A))))
+    else:
+        Misc.SendMessage("No Poison Pots",48)

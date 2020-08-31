2360d03c77f27df643cef83855bd838711637ba4
matsamilla <45471473+matsamilla@users.noreply.github.com>
2020-08-31 09:57:07 -0600

    Update misc_RecallHome.py

--- misc_RecallHome.py
+++ misc_RecallHome.py
@@ -15,3 +15,10 @@ if pname == "toonname":
 # Non Mage Toon (name no spaces all lowercase)
 elif pname == "toonname2":
     # Record opening book and recalling home in new script, then copy/paste here:
+# mage toon (name no spaces all lowercase)
+elif pname == "toonname3":
+    Spells.CastMagery('Recall')
+    Target.WaitForTarget(1500,False)
+    Target.TargetExecute(0x43286FAE) # Change to serialID of your recall book
+
+# to add more, copy-paste one of the elif's (mage or non mage) below last.

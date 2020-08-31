359d4c845effff55fe665e75536760136d723851
matsamilla <45471473+matsamilla@users.noreply.github.com>
2020-08-31 09:51:44 -0600

    Update misc_RecallHome.py

--- misc_RecallHome.py
+++ misc_RecallHome.py
@@ -6,7 +6,7 @@ Target.ClearLastandQueue()
 if Player.Name == "ToonName":
     Spells.CastMagery('Recall')
     Target.WaitForTarget(1500,False)
-    Target.TargetExecute(0x43286FAE)
+    Target.TargetExecute(0x43286FAE) # Change to serialID of your recall book
 # Non Mage Toon
 elif Player.Name == "ToonName":
     # Record opening book and recalling home then place here:

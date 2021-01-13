0705f413c65b595ddbb4bb0926c7f7008fcad466
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-01-13 16:38:55 -0700

    Create sallos_SmartPot.py

---
+++ sallos_SmartPot.py
@@ -0,0 +1,12 @@
+# Sallos SmartPot by MatsaMilla
+# Drinks cure if poisoned, otherwise drinks healV
+if Player.Poisoned:
+    Player.ChatSay( 1 , '[drink greatercurepotion')
+    Misc.Pause(100)
+    if Journal.Search( 'You do not have any of those potions.'):
+        Player.HeadMessage(msgColor, "No Cure pots!")
+else:
+    Player.ChatSay( 1 , '[drink greaterhealpotion')
+    Misc.Pause(100)
+    if (Journal.Search( 'You do not have any of those potions.'):
+        Player.HeadMessage(msgColor, "No Heal pots!")

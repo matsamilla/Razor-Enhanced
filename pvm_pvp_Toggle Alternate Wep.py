458f0071f096a08c4f3b77df578c3ed83767f2c0
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-02-16 23:22:08 -0700

    Add files via upload

---
+++ pvm_pvp_Toggle Alternate Wep.py
@@ -0,0 +1,33 @@
+# Toggle alternate wep by MatsaMilla
+# 
+
+leftHand = Player.GetItemOnLayer('LeftHand')
+rightHand = Player.GetItemOnLayer('RightHand')
+
+if Misc.CheckSharedValue(Player.Name + 'wep2'):
+    tempwep = Items.FindBySerial(Misc.ReadSharedValue(Player.Name + 'wep2'))
+    if tempwep:
+        wep = tempwep.Serial
+    else:
+        Player.HeadMessage(66,'Target New Alternate Wep')
+        wep = Target.PromptTarget()
+        Misc.SetSharedValue(Player.Name + 'wep2', wep)
+        Player.HeadMessage(66, 'Wep Set')
+else:
+    Player.HeadMessage(66,'Target Alternate Wep')
+    wep = Target.PromptTarget()
+    Misc.SetSharedValue(Player.Name + 'wep2', wep)
+    Player.HeadMessage(66, 'Wep Set')
+    
+
+if rightHand:
+    if rightHand.Serial == wep:
+        Items.Move(wep,Player.Backpack.Serial,0)
+        
+if leftHand:
+    if leftHand.Serial == wep:
+        Items.Move(wep,Player.Backpack.Serial,0)
+    else:
+       Player.EquipItem(wep)
+else:
+    Player.EquipItem(wep)

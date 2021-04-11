8e2a2540d119de331fb8cc4c6273a6842bb6ffb6
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-04-10 23:38:19 -0600

    Update cast_RecallHome.py

--- cast_RecallHome.py
+++ cast_RecallHome.py
@@ -1,28 +1,91 @@
 # Recall Home by MatsaMilla
-# Insert name without spaces and all lowercase.
-# Example: "Player Name" needs to be "playername"
+# version 2.0 4/10/21 - revamped, easier setup and checks for regs - recalls by charge if no regs
+# Setup:
+#   • Insert name without spaces and all lowercase.
+#     • Example: "Player Name" needs to be "playername"
+#   • runebook serial
+#   • runeNumber (posotion of rune in book, 1-16)
+#   • to add more, copy-paste one of the "elif" below last.
 
-msgcolor = 62
-openRunebookPause = 150
+# True = Mage will not open book, home rune set to default
+targetBook = True
+
+#ignore this line
 pname = Player.Name.lower().replace(' ', '')
 
-Target.ClearLastandQueue()
-# mage toon (name no spaces all lowercase)
-if pname == "toonname":
-    Spells.CastMagery('Recall')
-    Target.WaitForTarget(1500,False)
-    Target.TargetExecute(0x43286FAE) # Change to serialID of your recall book
-# Non Mage Toon (name no spaces all lowercase)
-elif pname == "toonname2":
-    # Record opening book and recalling home in new script, then copy/paste over the lines below:
-    Items.UseItem(0x423A667B)
-    Misc.Pause(openRunebookPause)
-    Gumps.WaitForGump(1431013363, 10000)
-    Gumps.SendAction(1431013363, 2)
-# mage toon (name no spaces all lowercase)
-elif pname == "toonname3":
-    Spells.CastMagery('Recall')
-    Target.WaitForTarget(1500,False)
-    Target.TargetExecute(0x43286FAE) # Change to serialID of your recall book
+if pname == "matsamilla":
+    runebook = 0x43286FAE
+    runeNumber = 1
+    
+elif pname == "toonname":
+    runebook = 0x402F6431
+    runeNumber = 5
+    
+elif pname == "othertoonname":
+    runebook = 0x40c0aa9a
+    runeNumber = 7
+    
+#***************************************************************#
+    
+def recallMagery(book, rune):
+    if targetBook:
+        Spells.CastMagery('Recall')
+        Target.WaitForTarget(1500,False)
+        Target.TargetExecute(book)
+        
+    else:
+        rune = rune -1
+        rune = 5 + (6 * rune)
+        if Gumps.CurrentGump() != 1431013363:
+            Items.UseItem(book)
+        Gumps.WaitForGump(1431013363, 1000)
+        Gumps.SendAction(1431013363, rune)
+    
+def recallCharge(book, rune):
+    rune = rune -1
+    rune = 2 + (6 * rune)
+    if Gumps.CurrentGump() != 1431013363:
+        Items.UseItem(book)
+    Gumps.WaitForGump(1431013363, 1000)
+    Gumps.SendAction(1431013363, rune)
+    
+def FindItem( itemID, container, color = -1, ignoreContainer = [] ):
+    '''
+    Searches through the container for the item IDs specified and returns the first one found
+    Also searches through any subcontainers, which Misc.FindByID() does not
+    '''
 
-# to add more, copy-paste one of the elif's (mage or non mage) below last.
+    ignoreColor = False
+    if color == -1:
+        ignoreColor = True
+
+    if isinstance( itemID, int ):
+        foundItem = next( ( item for item in container.Contains if ( item.ItemID == itemID and ( ignoreColor or item.Hue == color ) ) ), None )
+    elif isinstance( itemID, list ):
+        foundItem = next( ( item for item in container.Contains if ( item.ItemID in itemID and ( ignoreColor or item.Hue == color ) ) ), None )
+    else:
+        raise ValueError( 'Unknown argument type for itemID passed to FindItem().', itemID, container )
+
+    if foundItem != None:
+        return foundItem
+
+    subcontainers = [ item for item in container.Contains if ( item.IsContainer and not item.Serial in ignoreContainer ) ]
+    for subcontainer in subcontainers:
+        foundItem = FindItem( itemID, subcontainer, color, ignoreContainer )
+        if foundItem != None:
+            return foundItem
+            
+def checkRegs():
+    if (FindItem(0x0F7A , Player.Backpack) and FindItem(0x0F86 , Player.Backpack) and FindItem(0x0F7B , Player.Backpack) ):
+        return True
+    else:
+        return False
+
+    
+try:
+    if Player.GetRealSkillValue('Magery') > 50 and checkRegs():
+        recallMagery(runebook, runeNumber)
+    else:
+        recallCharge(runebook, runeNumber)
+except:
+    Player.HeadMessage(33,'Toon Not Set Up')

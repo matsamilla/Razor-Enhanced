adbe2724f7ebc74af74b9288c3c71d12a25fe3ec
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-04-29 09:46:58 -0600

    Update sallos_SmartPot.py

--- sallos_SmartPot.py
+++ sallos_SmartPot.py
@@ -4,9 +4,9 @@ if Player.Poisoned:
     Player.ChatSay( 1 , '[drink greatercurepotion')
     Misc.Pause(100)
     if Journal.Search( 'You do not have any of those potions.'):
-        Player.HeadMessage(msgColor, "No Cure pots!")
+        Player.HeadMessage(33, "No Cure pots!")
 else:
     Player.ChatSay( 1 , '[drink greaterhealpotion')
     Misc.Pause(100)
     if Journal.Search( 'You do not have any of those potions.'):
-        Player.HeadMessage(msgColor, "No Heal pots!")
+        Player.HeadMessage(33, "No Heal pots!")

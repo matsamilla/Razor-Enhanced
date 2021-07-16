37ffc82cc642d2c414de2459da2caa898275c0ad
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-07-16 14:43:04 -0600

    Update pvm_pvp_Swap or Toggle Wep.py

--- pvm_pvp_Swap or Toggle Wep.py
+++ pvm_pvp_Swap or Toggle Wep.py
@@ -2,12 +2,10 @@
 # Swaps between weps you target, last targeted is first equipted
 # if only one weapon targeted, acts as a wep toggle.
 
-# Version 3.0: Works better with 1 handed & shields
-# updated 7-15-21
+# Version 3.1, updated 7/16/21
 
 leftHand = Player.GetItemOnLayer('LeftHand')
 rightHand = Player.GetItemOnLayer('RightHand')
-twoHandedWeps = [ 0x0F4D,0x143E,0x0F4B,0x0F49,0x13FB,0x0F43,0x0F45,0x1443,0x0F43,0x0F62,0x1403,0x1439,0x0E89,0x13F8,0x0DF0,0x0E81,0x13B2,0x26C2,0x0F50,0x13FD ]
 shields = [ 0x1B76,0x1B74,0x1B7B,0x1B73,0x1B72,0x1B79,0x1B7A ]
 
 def setWeps(text, multiple=True):
@@ -60,7 +58,7 @@ def swampWeps():
     
     wep = Items.FindBySerial(weplist[lastPos])
     
-    if wep.ItemID in twoHandedWeps:
+    if wep.IsTwoHanded:
         Misc.SendMessage('Two Handed')
         if rightHand:
             Items.Move(rightHand.Serial,Player.Backpack.Serial,0)
@@ -120,7 +118,7 @@ def swampWeps():
         weplist = Misc.ReadSharedValue(Player.Name + 'weplist')
         
 def toggleWep():
-    if wep.ItemID in twoHandedWeps:
+    if wep.IsTwoHanded:
         Misc.SendMessage('Two Handed')
         if rightHand:
             Items.Move(rightHand.Serial,Player.Backpack.Serial,0)

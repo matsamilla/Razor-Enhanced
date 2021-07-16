26de4ef929a893804aa87295b4dd1196422c7706
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-07-15 23:48:02 -0600

    Update pvm_pvp_Toggle Alternate Wep.py

--- pvm_pvp_Toggle Alternate Wep.py
+++ pvm_pvp_Toggle Alternate Wep.py
@@ -1,39 +1,78 @@
 # Toggle alternate wep by MatsaMilla
-# 
+# updated 7-15-21
 
 leftHand = Player.GetItemOnLayer('LeftHand')
 rightHand = Player.GetItemOnLayer('RightHand')
 
+twoHandedWeps = [ 0x0F4D,0x143E,0x0F4B,0x0F49,0x13FB,0x0F43,0x0F45,0x1443,0x0F43,0x0F62,0x1403,0x1439,0x0E89,0x13F8,0x0DF0,0x0E81,0x13B2,0x26C2,0x0F50,0x13FD ]
+shields = [ 0x1B76,0x1B74,0x1B7B,0x1B73,0x1B72,0x1B79,0x1B7A ]
+
 if Misc.CheckSharedValue(Player.Name + 'wep2'):
     tempwep = Items.FindBySerial(Misc.ReadSharedValue(Player.Name + 'wep2'))
     if tempwep:
-        wep = tempwep.Serial
+        wep = tempwep
     else:
-        Player.HeadMessage(66,'Target New Alternate Wep')
-        wep = Target.PromptTarget()
-        Misc.SetSharedValue(Player.Name + 'wep2', wep)
+        Player.HeadMessage(66,'Target New Wep')
+        wep = Items.FindBySerial( Target.PromptTarget() )
+        Misc.SetSharedValue(Player.Name + 'wep2', wep.Serial)
         Player.HeadMessage(66, 'Wep Set')
 else:
-    Player.HeadMessage(66,'Target Alternate Wep')
-    wep = Target.PromptTarget()
-    Misc.SetSharedValue(Player.Name + 'wep2', wep)
+    Player.HeadMessage(66,'Target Wep')
+    wep = Items.FindBySerial( Target.PromptTarget() )
+    Misc.SetSharedValue(Player.Name + 'wep2', wep.Serial)
     Player.HeadMessage(66, 'Wep Set')
     
-
-if rightHand:
-    if rightHand.Serial == wep:
-        Items.Move(wep,Player.Backpack.Serial,0)
-#    else:
-#        Items.Move(rightHand.Serial,Player.Backpack.Serial,0)
-#        Misc.Pause(600)
-#        Player.EquipItem(wep)
+def toggleWep():
+    if wep.ItemID in twoHandedWeps:
+        Misc.SendMessage('Two Handed')
+        if rightHand:
+            Items.Move(rightHand.Serial,Player.Backpack.Serial,0)
+            Misc.Pause(600)
+        if leftHand:
+            if leftHand.Serial == wep.Serial:
+                Items.Move(wep,Player.Backpack.Serial,0)
+                Misc.Pause(600)
+            else:
+                Items.Move(leftHand.Serial,Player.Backpack.Serial,0)
+                Misc.Pause(600)
+                Player.EquipItem(wep)
+        else:
+            Player.EquipItem(wep)
+            
+    elif wep.ItemID in shields:
+        Misc.SendMessage('Shield')
+        if leftHand:
+            if leftHand.Serial == wep.Serial:
+                Items.Move(wep,Player.Backpack.Serial,0)
+            else:
+                Items.Move(leftHand.Serial,Player.Backpack.Serial,0)
+                Misc.Pause(600)
+                Player.EquipItem(wep)
+        else:
+            Player.EquipItem(wep) 
         
-if leftHand:
-    if leftHand.Serial == wep:
-        Items.Move(wep,Player.Backpack.Serial,0)
+        
     else:
-        Items.Move(leftHand.Serial,Player.Backpack.Serial,0)
-        Misc.Pause(600)
-        Player.EquipItem(wep)
-else:
-    Player.EquipItem(wep)
+        if rightHand:
+            if rightHand.Serial == wep.Serial:
+                Items.Move(wep,Player.Backpack.Serial,0)
+            else:
+                Items.Move(rightHand,Player.Backpack.Serial,0)
+                Misc.Pause(600)
+                Player.EquipItem(wep)
+                
+        if leftHand:
+            if leftHand.Serial == wep.Serial:
+                Items.Move(wep,Player.Backpack.Serial,0)
+            elif leftHand.ItemID in shields:
+                Player.EquipItem(wep)
+            else:                
+                Items.Move(leftHand,Player.Backpack.Serial,0)
+                Misc.Pause(600)
+                Player.EquipItem(wep)
+        
+        else:
+            Player.EquipItem(wep)
+        
+toggleWep()
+

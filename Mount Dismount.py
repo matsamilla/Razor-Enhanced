5218ee41fac45e8d8804f0afe93eca0e95591e18
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-08-28 10:33:26 -0600

    Update Mount Dismount.py

--- Mount Dismount.py
+++ Mount Dismount.py
@@ -1,62 +1,61 @@
-# Mount/Dismount/Attack script by MatsaMilla
-# last updated 5/14/20
+delayTime = 600
 
-self = Player.Serial
-dragTime = 600
-
-# I use this for tamers, will All Kill & target last target when you dismount
-# then call pet to you until close enough to mount again.
-dismountAttackList = [ "MatsaMilla", "MGD" ] # good for tamers
-
-# I use this for toons with beetles & toons with stealth
-dismountOnly = [ "Matsa" , "EmGD"  ]
-
-if Player.Name == "MatsaMilla":
-    mountID = 0x01237E94
+# add (Name, PlayerSerial, MountSerial, True/False) to list 
+#   True if you want toon to DISMOUNT + ALL KILL + TARGET LAST
+dismountList = [ 
+#   (PlayerName, PlayerSerial, MountSerial, True/False)
+    ("MGD" , 0x0000B71C , 0x003FAA39 , True ), 
+    ("Moltar" , 0x003C518F , 0x0007E386 , True ), 
+    ("EmGD" , 0x00255122 , 0x00215F22 , False),
+    ("UselessGuy" , 0x0022FE20 , 0x0006EB2A , False ),
+    ("Matsa" , 0x000470E6 , 0x0000 , False ),
+    ("Matsa-" , 0x001DF0B7 , 0x0028E693 , False ),
+    ("Thicc" , 0x0016218A , 0x0001E6B3 , False),
+    ("MatsaAxa", 0x0000 , 0x00285C67 , False),
+    ("Nalfein" , 0x000B519E , 0x00823AEA , False ),
+    ("MatsaMilla" , 0x0002DC9C , 0x0007A37B , False ),
+    ("useless", 0x000AE1A2 , 0x000803D8 , False),
+] 
     
-elif Player.Name == "Matsa":
-    mountID = 0x00121234
     
-elif Player.Name == "MGD":
-    mountID = 0x00121234
-    
-elif Player.Name == "EmGD":
-    mountID = 0x00121234
-    
-################## no touchy below ########################
-
-def dismount():
+def dismount(mountSerial, attack):
     if Player.Mount:
-        Mobiles.UseMobile(self)
-        Misc.Pause(100)
-        mount = Mobiles.FindBySerial(mountID)
-        if mount.Body == 0x0317: #mountID != None:
-            Misc.WaitForContext(mountID, 1500)
-            Misc.ContextReply(mountID, "Open Backpack")
+        if attack:
+            lastTarget = Mobiles.FindBySerial( Target.GetLast() )
+            if lastTarget:
+                if lastTarget != Player.Serial:
+                    if Player.DistanceTo(lastTarget) < 12:
+                        Mobiles.UseMobile(Player.Serial)
+                        Misc.Pause(100)
+                        Player.ChatSay(33, 'All Kill')
+                        Target.WaitForTarget(1500)
+                        Target.TargetExecute(lastTarget)
     
-    elif mountID != None:
-        Mobiles.UseMobile(mountID)
-        
-        
-def dismountAttack():
-    attackMount = Mobiles.FindBySerial(mountID)
-    if Player.Mount:
-        Mobiles.UseMobile(self)
-        Misc.Pause(100)
-        Player.ChatSay(33, 'All Kill')
-        Target.WaitForTarget(1500)
-        Target.Last()
-    else:
-        while Player.DistanceTo(attackMount) > 1:
+        else:
+            Mobiles.UseMobile(Player.Serial)
+            Misc.Pause(120)
+            mount = Mobiles.FindBySerial(mountSerial)
+            if mount:
+                if mount.Body == 0x0317:
+                    Misc.WaitForContext(mount, 1500)
+                    Misc.Pause(150)
+                    if Player.Visible:
+                        Misc.ContextReply(mountSerial, "Open Backpack")
+                    else:
+                        Misc.ContextReply(mountSerial, "Open Backpack")
+    
+    elif mountSerial != None:
+        mount = Mobiles.FindBySerial(mountSerial)
+        while Player.DistanceTo(mount) > 1:
             if Timer.Check('MountTimer') == False:
-                Player.ChatSay(66, 'All Come')
+                Player.ChatSay(66, mount.Name + ' Come')
                 Timer.Create('MountTimer', 500)
             Misc.Pause(50)
-        Mobiles.UseMobile(mountID)
+        Mobiles.UseMobile(mountSerial)
+
         
-        
-if Player.Name in dismountAttackList: 
-    dismountAttack()
-    
-elif Player.Name in dismountOnly:
-    dismount()
+for d in dismountList: 
+    if Player.Serial == d[1]:
+        dismount(d[2], d[3])
+        break
+

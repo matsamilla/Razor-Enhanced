a49f5eeb53e991aa0adbbd1928092375a39e1877
matsamilla <45471473+matsamilla@users.noreply.github.com>
2021-08-28 10:26:58 -0600

    Update and rename pvm_pvp_Pot_Buff Refresh.py to Potion_Buff + Refresh.py

--- Potion_Buff + Refresh.py
+++ Potion_Buff + Refresh.py
@@ -4,7 +4,7 @@ chugtime = 650
 journalTimeout = 100
 msgColor = 66
 noBow = True
-bows = [0x13B2,0x26C2,0x0F50,0x13FD]
+bows = [0x13B2,0x26C2,0x0F50,0x13FD,0x0A12,0x0F6B] # 0x0A12,0x0F6B = torch
 
 if not leftHand:
     noBow = True
@@ -17,10 +17,18 @@ def str():
     Player.ChatSay( 1 , '[drink GreaterStrengthPotion ')
     Misc.Pause( journalTimeout )
     if Journal.Search( 'You do not have any of those potions.'):
-        Player.HeadMessage(msgColor, "No Strenth pots!")
+        Player.HeadMessage(msgColor, "No Strength pots!")
         Journal.Clear()
     else:
         Misc.Pause(chugtime)
+        
+def strPot():
+    whitePot = Items.FindByID(0x0F09,0,Player.Backpack.Serial,True)
+    if whitePot:
+        Items.UseItem(whitePot)
+        Misc.Pause(chugtime)
+    else:
+        Player.HeadMessage(msgColor, "No Strength pots!")
         
 def agil():
     Player.ChatSay( 1 , '[drink GreaterAgilityPotion')
@@ -31,19 +39,35 @@ def agil():
     else:
         Misc.Pause(chugtime)
         
+def agilPot():
+    bluePot = Items.FindByID(0x0F08,0,Player.Backpack.Serial,True)
+    if bluePot:
+        Items.UseItem(bluePot)
+        Misc.Pause(chugtime)
+    else:
+        Player.HeadMessage(msgColor, "No Agility pots!")
+
+        
 def refresh():
     Player.ChatSay( 1 , '[drink totalrefreshpotion')
     Misc.Pause(100)
     if Journal.Search( 'You do not have any of those potions.'):
         Player.HeadMessage(msgColor, "No Refresh pots!")
         Journal.Clear()
+#    else:
+#        Misc.Pause(chugtime)
+
+def refreshPot():
+    redPot = Items.FindByID(0x0F0B,0,Player.Backpack.Serial,True)
+    if redPot:
+        Items.UseItem(redPot)
     else:
-        Misc.Pause(chugtime)
+        Player.HeadMessage(msgColor, "No Refresh pots!")
         
 def clearHands():
     if leftHand and noBow:
         Player.UnEquipItemByLayer('LeftHand')
-        Misc.Pause(650)
+        Misc.Pause(chugtime)
         return True
     else:
         return False
@@ -83,5 +107,41 @@ def buffUp():
             reArm()
     else:
         Player.HeadMessage(msgColor, 'Buffed and full stam')
+        
+def buffUpPots():
+    if Player.Str <= 100:
+        if clearHands():
+            disarmed = True
+        else:
+            disarmed = False
+        strPot()
+        agilPot()
+        refreshPot()
+        if disarmed:
+            reArm()
+    elif Player.Dex <= 100:
+        if clearHands():
+            disarmed = True
+        else:
+            disarmed = False
+        strPot()
+        agilPot()
+        refreshPot()
+        if disarmed:
+            reArm()
+    elif Player.Stam <= Player.StamMax:
+        if clearHands():
+            disarmed = True
+        else:
+            disarmed = False
+        refreshPot()
+        if disarmed:
+            reArm()
+    else:
+        Player.HeadMessage(msgColor, 'Buffed and full stam')
 
-buffUp()
+
+if Misc.ShardName() == "Ultima Forever":
+    buffUp()
+else:
+    buffUpPots()

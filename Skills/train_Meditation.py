515a1b719bbe9bb4c775d8f353a93a148d6ad1ad
matsamilla <45471473+matsamilla@users.noreply.github.com>
2022-04-05 15:02:02 -0600

    Update train_Meditation.py

--- Skills/train_Meditation.py
+++ Skills/train_Meditation.py
@@ -1,12 +1,18 @@
 # train meditation to GM by Mastamilla
-# purchase magical wizard hat from mage shop 
-# start with wizard hat in backpack
+# purchase magical wizard hat from mage shop
 
 wizardHat = Items.FindByID( 0x1718 , -1 , Player.Backpack.Serial )
 
-while Player.GetRealSkillValue("Meditation") < 100:
-    if Player.Mana == Player.ManaMax:
-        Items.Move(wizardHat, Player.Backpack.Serial, 0)
-        Misc.Pause(2000)
-        Player.EquipItem(wizardHat)
-        Misc.Pause(600)
+if not wizardHat:
+    wizardHat = Player.GetItemOnLayer('Head')
+    
+if wizardHat:
+    while Player.GetRealSkillValue("Meditation") < 100:
+        if Player.Mana >= Player.ManaMax:
+            Items.Move(wizardHat, Player.Backpack.Serial, 0)
+            Misc.Pause(2000)
+            Player.EquipItem(wizardHat)
+            Misc.Pause(600)
+            
+else:
+    Player.HeadMessage(33,'No wizard hat found.')

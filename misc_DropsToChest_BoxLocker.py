2b391f4c2db43331e9341fe37b5303ab9a51b1c0
matsamilla <45471473+matsamilla@users.noreply.github.com>
2022-07-29 17:05:39 -0600

    Update misc_DropsToChest_BoxLocker.py

--- misc_DropsToChest_BoxLocker.py
+++ misc_DropsToChest_BoxLocker.py
@@ -1,41 +1,15 @@
 # Move big item to trap pouch & announce by MatsaMilla
-# version 2.2 - 8/30/21
-
-############ setup ########################
-
-lockBoxAfterDrop = False
-
-# list of items to toss in box
-itemsToBox = [
-    0x2a7c, #egg
-    0x47E6, #egg
-    0x2AA4, #relic
-    0x14F0, #ps
-    0x1AE1, #skull
-    0x35DA, #event frag
-    0x0F21, #portal frag
-    0x2dd5, #perk
-    0x1f18, #perk
-    0x2da3, #perk
-    0x166e, #perk
-    0x26BB, #perk
-    0x139B, #perk
-    0x0FBE, #perk
-    0x2260, #skill scroll
-    0x422B, #crab bite
-    0x0C65, #dragon fruit
-    
-]
-
-# add specific items to not box here (like blessed relics)
-ignoreSerials = [0x402E5274]
-
-#########################################################
+# version 2.2 - fixed if you get drop after locked
 
 #standard imports
 from System.Collections.Generic import List
 from System import Byte
 import sys
+
+lockBoxAfterDrop = True
+
+# will not box item if serial in this list, like blessed relics
+ignoreSerials = [0x402E5274]
 
 # delay for login
 Misc.Pause(2500)
@@ -46,8 +20,32 @@ backpack = Items.FindBySerial(Player.Backpack.Serial)
 key = 0x100E
 box = False
 
+#dragon egg
+eggList = [0x2a7c,0x47E6]
+
+#meta relics
+relicsList  = [0x2AA4]
+
+#power scrolls
+ps = 0x14F0
+
+#skulls
+skullList = [0x1AE1]
+
+#crystals
+crystalsList = [
+0x35DA, #event crystal
+0x0F21, #portal frag
+]
+
+perks = [ 0x2dd5,0x1f18,0x2da3,0x166e,0x26BB,0x139B,0x0FBE ]
+
+# only boxes tame SSs, blaze color
+ss = [ 0x2260 ]
+
 #box types
 trapBox = [0x9aa,0x9a9,0xe3e,0xe3c,0xe42,0xe43,0x280b,0xe7d,0xe7e,0xe3f,0xe3d,0x280c]
+
 
 for b in backpack.Contains:
     if b.ItemID in trapBox:
@@ -57,36 +55,23 @@ for b in backpack.Contains:
         
 moveList = []
 
-def FindItem( itemID, container, color = -1, ignoreContainer = [] ):
-    '''
-    Searches through the container for the item IDs specified and returns the first one found
-    Also searches through any subcontainers, which Misc.FindByID() does not
-    '''
-
-    ignoreColor = False
-    if color == -1:
-        ignoreColor = True
-
-    if isinstance( itemID, int ):
-        foundItem = next( ( item for item in container.Contains if ( item.ItemID == itemID and ( ignoreColor or item.Hue == color ) ) ), None )
-    elif isinstance( itemID, list ):
-        foundItem = next( ( item for item in container.Contains if ( item.ItemID in itemID and ( ignoreColor or item.Hue == color ) ) ), None )
-    else:
-        raise ValueError( 'Unknown argument type for itemID passed to FindItem().', itemID, container )
-
-    if foundItem != None:
-        return foundItem
-
-    subcontainers = [ item for item in container.Contains if ( item.IsContainer and not item.Serial in ignoreContainer ) ]
-    for subcontainer in subcontainers:
-        foundItem = FindItem( itemID, subcontainer, color, ignoreContainer )
-        if foundItem != None:
-            return foundItem
-
 def dropWatch():
     for i in backpack.Contains:
-        if i.ItemID in itemsToBox:
-            moveList.append(i.Serial)        
+        if i.ItemID in eggList:
+            moveList.append(i.Serial)
+        elif i.ItemID in relicsList:
+            moveList.append(i.Serial)
+        elif (i.ItemID == ps and i.Hue == 0x0481):
+            moveList.append(i.Serial)
+        elif i.ItemID in skullList and i.Hue == 0x0000:
+            moveList.append(i.Serial)
+        elif i.ItemID in crystalsList: 
+            if i.Hue == 0x0489 or i.ItemID == 0x35DA:
+                moveList.append(i.Serial)
+        elif i.ItemID in perks:
+            moveList.append(i.Serial)
+        elif i.Hue == 0x0489 and i.ItemID in ss:
+            moveList.append(i.Serial)
             
     for m in moveList:
         if m in ignoreSerials:
@@ -98,6 +83,7 @@ def dropWatch():
             lockBox()
         
 def boxItems():
+    Journal.Clear()
     for i in moveList:
         Items.Move(i, box, 0)
         Misc.Pause(dragtime)
@@ -107,14 +93,18 @@ def boxItems():
         if itemName:
             Player.HeadMessage(msgColor, itemName)
             hotItem = Items.FindBySerial(i)
+            Misc.Pause(50)
             if hotItem.Container != box.Serial:
                 Player.HeadMessage(33,'NOT IN BOX')
-            Player.ChatParty(itemName + ' In Box')
+                if Journal.Search('It appears to be locked.'):
+                    unlockBox()
+            else:
+                Player.ChatParty(itemName + ' In Box')
     del moveList[:]
         
 def lockBox():
     Journal.Clear()
-    boxKey = FindItem(key,Player.Backpack)
+    boxKey = Items.FindByID(key,-1,Player.Backpack.Serial,True)
     if boxKey:
         Items.UseItem(boxKey)
         Target.WaitForTarget(1500)
@@ -127,12 +117,29 @@ def lockBox():
             lockBox()
     else:
         Player.HeadMessage(msgColor, 'No Key!\n!!NOT LOCKED!!')
+        
+def unlockBox():
+    Journal.Clear()
+    boxKey = Items.FindByID(key,-1,Player.Backpack.Serial,True)
+    if boxKey:
+        Items.UseItem(boxKey)
+        Target.WaitForTarget(1500)
+        Target.TargetExecute(box)
+        Misc.Pause(dragtime)
+        if Journal.Search('You unlock it.'):
+            Player.HeadMessage(msgColor, 'Box unLocked!!')
+            Player.ChatParty('Box unLocked!!')
+    else:
+        Player.HeadMessage(msgColor, 'No Key!\n!!Still UnLOCKED!!')
+    
+    boxItems()
+    
 
 Journal.Clear()
 while True:
     if not box:
         Misc.Pause(500)
         Misc.SendMessage('No trap box, stopping script', 33)
-        Misc.ScriptStop('misc_Drops To Chest.py')
+        sys.exit()
     Misc.Pause(50)
     dropWatch()   
